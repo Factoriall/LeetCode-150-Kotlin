19c060fe147aa632a39432b5d115dc72f816414b
fac.toriall <fac.toriall@kakaocorp.com>
2026-03-29 17:20:10 +0900

    feat: 58 improve readability

--- src/leetcode/Top150/Q0058_Length_of_Last_Word/Solution.kt
+++ src/leetcode/Top150/Q0058_Length_of_Last_Word/Solution.kt
@@ -2,18 +2,23 @@ package leetcode.Top150.Q0058_Length_of_Last_Word
 
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        var lastWordEndIdx = -1
+        return getLength(s, getEndIndex(s))
+    }
 
+    private fun getEndIndex(s: String): Int {
         for(idx in s.length - 1 downTo 0) {
             val char = s[idx]
-            if(char != ' ' && lastWordEndIdx == -1) {
-                lastWordEndIdx = idx
-            }
-            if(char == ' ' && lastWordEndIdx != -1) {
-                return lastWordEndIdx - idx
-            }
+            if(char != ' ') return idx
         }
 
-        return lastWordEndIdx + 1
+        return -1
+    }
+
+    private fun getLength(s: String, end: Int): Int {
+        for(idx in end - 1 downTo 0) {
+            val char = s[idx]
+            if(char == ' ') return end - idx
+        }
+        return end + 1
     }
 }
